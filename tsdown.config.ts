486cf0b8cef2076df142cb92006fcbc38783958c
andrewdku <notspammail@icloud.com>
2026-03-23 12:08:23 +0200

    build: update tsdown config

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,14 +1,15 @@
 import { defineConfig } from "tsdown";
 
 export default defineConfig({
-    format: ["esm", "cjs"],
-    entry: ["src/index.ts"],
-    skipNodeModulesBundle: true,
-    removeNodeProtocol: false,
-    minify: false,
     cjsDefault: true,
     clean: true,
-    shims: true,
     dts: true,
-    target: false,
+    entry: ["src/index.ts"],
+    format: ["cjs"],
+    minify: false,
+    platform: "node",
+    removeNodeProtocol: false,
+    shims: true,
+    skipNodeModulesBundle: true,
+    target: "node22",
 });
